42c1d7575957fc8f9f2eb96b26ecc519ea277254
Man From Nowhere <86987720+ManFNowhere@users.noreply.github.com>
2023-04-26 01:32:44 +0200

    v0.3

--- php-webapp/www/script/register.php
+++ php-webapp/www/script/register.php
@@ -10,3 +10,19 @@ $conn = mysqli_connect($host, $user, $password, $database);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
+//get input from register html
+$fullname = $_POST["fullname"];
+$username = $_POST["username"];
+$email = $_POST["email"];
+$telpNummer = $_POST["telpNummer"];
+$password = $_POST["password"];
+
+//echo ($fullname.$username.$email.$telpNummer.$password);
+if(mysqli_query($conn, "INSERT INTO demo (fullname, username, email, telpNummer,password) VALUES
+('$fullname','$username', '$email', $telpNummer, '$password')")){
+    echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+mysqli_close($conn);
