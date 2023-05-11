1cddd6e4112f3011d093884a61a13a3004af9ad8
Man From Nowhere <86987720+ManFNowhere@users.noreply.github.com>
2023-05-11 14:54:42 +0200

    tfw v0.9
    Add :
    - duplikasi email di validation dan demo db
    
    bug :
    - smpt
    - Super privilage for event_scheduler

--- php-webapp/www/script/registerHandler.php
+++ php-webapp/www/script/registerHandler.php
@@ -24,26 +24,44 @@ $password = password_hash($_POST["password"],PASSWORD_DEFAULT);
 $token = bin2hex(openssl_random_pseudo_bytes(32));
 $activationKey = bin2hex(openssl_random_pseudo_bytes(4));
 
+//query to check email is exist in demo
+$emailUserChecker = $conn -> query("SELECT * FROM demo WHERE email='".$email."'");
+$rowUser = $emailUserChecker->fetch_assoc();
 
-//echo ($fullname.$username.$email.$telpNummer.$password);
-if(mysqli_query($conn, "INSERT INTO validation (timestamp, fullname, username, email, telpNummer,password, token, activationKey) VALUES
-(CURRENT_TIMESTAMP,'$fullname','$username', '$email', $telpNummer, '$password','$token', '$activationKey')")){
-    $_SESSION['fullname']= $fullname;
-    $_SESSION['token'] = $token;
-    //send email verification
-    $message = "Hi $fullname! 
-    Account created here is the activation link http://localhost/registration/activate.php?token=$token";
-    mail($email,'Activation Link TfW 2023', $message, 'From: tmahdi@studenten.hs-bremerhaven.de ');
-    include 'simpleHeader.php';
-    echo ("<div><h2 class='u-text u-text-default u-title u-text-1'>Verification link sended to your Email(".$email.".)</h2></div>
-    <br><br><br><br><br><br><div><h3 class='u-text u-text-default'>Activation Key : ".$activationKey."</h3>
-    <a href='../validation.html'>Click here for validation link</a></div>
-    </div></section>");
-    include 'footer.php';
 
-    ##echo "New record created successfully";
+if(!is_array($rowUser)){
+  //query to check email is listed to be activ in validation db
+  $emailValidChecker = $conn -> query("SELECT * FROM validation WHERE email='".$email."'");
+  $rowValid = $emailValidChecker->fetch_assoc();
+  if(!is_array($rowValid)){
+    //echo ($fullname.$username.$email.$telpNummer.$password.$token);
+    if(mysqli_query($conn, "INSERT INTO validation (timestamp, fullname, username, email, telpNummer,password, token, activationKey) VALUES
+    (CURRENT_TIMESTAMP,'$fullname','$username', '$email', $telpNummer, '$password','$token', '$activationKey')")){
+        $_SESSION['fullname']= $fullname;
+        $_SESSION['token'] = $token;
+        //send email verification
+        $message = "Hi $fullname! 
+        Account created here is the activation link http://localhost/registration/activate.php?token=$token";
+        mail($email,'Activation Link TfW 2023', $message, 'From: tmahdi@studenten.hs-bremerhaven.de ');
+        include 'simpleHeader.php';
+        echo ("<div><h2 class='u-text u-text-default u-title u-text-1'>Verification link sended to your Email(".$email.".)</h2></div>
+        <br><br><br><br><br><br><div><h3 class='u-text u-text-default'>Activation Key : ".$activationKey."</h3>
+        <a href='../validation.html'>Click here for validation link</a></div>
+        </div></section>");
+        include 'footer.php';
+
+        ##echo "New record created successfully";
+    } else {echo "Error: " . $sql . "<br>" . mysqli_error($conn);}
+  }else{
+    echo 'Account waiting to be activ!!!';
+        echo '<script type="text/javascript">';
+        echo " alert('Account waiting to be activ!!!');window.location.href='../validation.html'";
+        echo '</script>';
+    }
 } else {
-  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  echo 'Account Exist!!!';
+        echo '<script type="text/javascript">';
+        echo " alert('Account Exist!!!');window.location.href='../register.html'";
+        echo '</script>';
 }
-
 
